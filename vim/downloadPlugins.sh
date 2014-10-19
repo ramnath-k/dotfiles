dce9c4f6c67c1aa48b8a66968e5e95a95475b361
ramnath-k <k.ramnath@gmail.com>
2014-10-19 06:42:29 +0530

    adding a download script for plugins

---
+++ vim/downloadPlugins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+cd ~/.vim/bundle
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/scrooloose/syntastic.git
+git clone https://github.com/tpope/vim-bundler.git
+git clone https://github.com/kchmck/vim-coffee-script.git
+git clone https://github.com/tpope/vim-endwise.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/tpope/vim-rails.git
+git clone https://github.com/vim-ruby/vim-ruby.git
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/tpope/vim-unimpaired.git
+

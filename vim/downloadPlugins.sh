ca8bca1bf70bac84a581549189e5fb3283fe1f49
Ramnath Krishnamurthy <ramnath@madstreetden.com>
2015-12-12 14:36:17 +0530

    script to add submodules for vim plugins to git

--- vim/downloadPlugins.sh
+++ vim/downloadPlugins.sh
@@ -1,14 +1,31 @@
 #!/bin/bash -e
-cd ~/.vim/bundle
-git clone https://github.com/kien/ctrlp.vim.git
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/scrooloose/syntastic.git
-git clone https://github.com/tpope/vim-bundler.git
-git clone https://github.com/kchmck/vim-coffee-script.git
-git clone https://github.com/tpope/vim-endwise.git
-git clone https://github.com/tpope/vim-fugitive.git
-git clone https://github.com/tpope/vim-rails.git
-git clone https://github.com/vim-ruby/vim-ruby.git
-git clone https://github.com/tpope/vim-surround.git
-git clone https://github.com/tpope/vim-unimpaired.git
+cd ~/.vim
+git submodule init
+git submodule add https://github.com/kien/ctrlp.vim.git
+git commit -m 'Added ctrlp-vim'
+git submodule add https://github.com/scrooloose/nerdtree.git
+git commit -m 'Added nerdtree'
+git submodule add https://github.com/scrooloose/syntastic.git
+git commit -m 'Added syntastic'
+git submodule add https://github.com/tpope/vim-bundler.git
+git commit -m 'Added vim-bundler'
+git submodule add https://github.com/tpope/vim-endwise.git
+git commit -m 'Added vim-endwise'
+git submodule add https://github.com/tpope/vim-fugitive.git
+git commit -m 'Added vim-fugitive'
+git submodule add https://github.com/tpope/vim-surround.git
+git commit -m 'Added vim-surround'
+git submodule add https://github.com/tpope/vim-unimpaired.git
+git commit -m 'Added vim-unimpaired'
+#git submodule add https://github.com/kchmck/vim-coffee-script.git
+#git commit -m 'Added vim-coffee-script'
+#git submodule add https://github.com/tpope/vim-rails.git
+#git commit -m 'Added vim-rails'
+#git submodule add https://github.com/vim-ruby/vim-ruby.git
+#git commit -m 'Added vim-ruby'
+
+# on a new system
+# cd ~/.vim
+# git submodule init
+# git submodule update
 

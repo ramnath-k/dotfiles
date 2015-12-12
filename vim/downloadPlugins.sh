dd8b4acb67e0bb5a046c1ed430558ae494cf32dc
Ramnath Krishnamurthy <ramnath@madstreetden.com>
2015-12-12 14:42:48 +0530

    code to init submodules in bundle dir

--- vim/downloadPlugins.sh
+++ vim/downloadPlugins.sh
@@ -1,27 +1,27 @@
 #!/bin/bash -e
 cd ~/.vim
 git submodule init
-git submodule add https://github.com/kien/ctrlp.vim.git bundle/
+git submodule add https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
 git commit -m 'Added ctrlp-vim'
-git submodule add https://github.com/scrooloose/nerdtree.git bundle/
+git submodule add https://github.com/scrooloose/nerdtree.git bundle/nerdtree
 git commit -m 'Added nerdtree'
-git submodule add https://github.com/scrooloose/syntastic.git bundle/
+git submodule add https://github.com/scrooloose/syntastic.git bundle/syntastic
 git commit -m 'Added syntastic'
-git submodule add https://github.com/tpope/vim-bundler.git bundle/
+git submodule add https://github.com/tpope/vim-bundler.git bundle/vim-bundler
 git commit -m 'Added vim-bundler'
-git submodule add https://github.com/tpope/vim-endwise.git bundle/
+git submodule add https://github.com/tpope/vim-endwise.git bundle/vim-endwise
 git commit -m 'Added vim-endwise'
-git submodule add https://github.com/tpope/vim-fugitive.git bundle/
+git submodule add https://github.com/tpope/vim-fugitive.git bundle/vim-fugitive
 git commit -m 'Added vim-fugitive'
-git submodule add https://github.com/tpope/vim-surround.git bundle/
+git submodule add https://github.com/tpope/vim-surround.git bundle/vim-surround
 git commit -m 'Added vim-surround'
-git submodule add https://github.com/tpope/vim-unimpaired.git bundle/
+git submodule add https://github.com/tpope/vim-unimpaired.git bundle/vim-unimpaired
 git commit -m 'Added vim-unimpaired'
-#git submodule add https://github.com/kchmck/vim-coffee-script.git bundle/
+#git submodule add https://github.com/kchmck/vim-coffee-script.git bundle/vim-coffee-script
 #git commit -m 'Added vim-coffee-script'
-#git submodule add https://github.com/tpope/vim-rails.git bundle/
+#git submodule add https://github.com/tpope/vim-rails.git bundle/vim-rails
 #git commit -m 'Added vim-rails'
-#git submodule add https://github.com/vim-ruby/vim-ruby.git bundle/
+#git submodule add https://github.com/vim-ruby/vim-ruby.git bundle/vim-ruby
 #git commit -m 'Added vim-ruby'
 
 # on a new system

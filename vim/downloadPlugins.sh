617c06a0e89e0f332cde1d4993c2a110c1be0721
Ramnath Krishnamurthy <ramnath@madstreetden.com>
2015-12-12 14:37:31 +0530

    cloning into bundle dir

--- vim/downloadPlugins.sh
+++ vim/downloadPlugins.sh
@@ -1,27 +1,27 @@
 #!/bin/bash -e
 cd ~/.vim
 git submodule init
-git submodule add https://github.com/kien/ctrlp.vim.git
+git submodule add https://github.com/kien/ctrlp.vim.git bundle/
 git commit -m 'Added ctrlp-vim'
-git submodule add https://github.com/scrooloose/nerdtree.git
+git submodule add https://github.com/scrooloose/nerdtree.git bundle/
 git commit -m 'Added nerdtree'
-git submodule add https://github.com/scrooloose/syntastic.git
+git submodule add https://github.com/scrooloose/syntastic.git bundle/
 git commit -m 'Added syntastic'
-git submodule add https://github.com/tpope/vim-bundler.git
+git submodule add https://github.com/tpope/vim-bundler.git bundle/
 git commit -m 'Added vim-bundler'
-git submodule add https://github.com/tpope/vim-endwise.git
+git submodule add https://github.com/tpope/vim-endwise.git bundle/
 git commit -m 'Added vim-endwise'
-git submodule add https://github.com/tpope/vim-fugitive.git
+git submodule add https://github.com/tpope/vim-fugitive.git bundle/
 git commit -m 'Added vim-fugitive'
-git submodule add https://github.com/tpope/vim-surround.git
+git submodule add https://github.com/tpope/vim-surround.git bundle/
 git commit -m 'Added vim-surround'
-git submodule add https://github.com/tpope/vim-unimpaired.git
+git submodule add https://github.com/tpope/vim-unimpaired.git bundle/
 git commit -m 'Added vim-unimpaired'
-#git submodule add https://github.com/kchmck/vim-coffee-script.git
+#git submodule add https://github.com/kchmck/vim-coffee-script.git bundle/
 #git commit -m 'Added vim-coffee-script'
-#git submodule add https://github.com/tpope/vim-rails.git
+#git submodule add https://github.com/tpope/vim-rails.git bundle/
 #git commit -m 'Added vim-rails'
-#git submodule add https://github.com/vim-ruby/vim-ruby.git
+#git submodule add https://github.com/vim-ruby/vim-ruby.git bundle/
 #git commit -m 'Added vim-ruby'
 
 # on a new system
